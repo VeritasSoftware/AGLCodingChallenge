d8195097862fff24eee9aacf13be6d5789d8d352
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-16 16:40:46 +1000

    added route for get api. code comment headers etc.

--- AGL.API/AGL.API/Controllers/PetsController.cs
+++ AGL.API/AGL.API/Controllers/PetsController.cs
@@ -6,18 +6,29 @@ using System.Threading.Tasks;
 
 namespace AGL.API.Controllers
 {
+    /// <summary>
+    /// Pets controller
+    /// </summary>
     [Route("api/[controller]")]
     public class PetsController : Controller
     {
         private readonly IPetsManager _petsManager;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="petsManager">The injected pets manager</param>
         public PetsController(IPetsManager petsManager)
         {
             _petsManager = petsManager ?? throw new ArgumentNullException(nameof(petsManager));
         }
 
-        // GET api/values
-        [HttpGet]
+        /// <summary>
+        /// Get pets by person gender endpoint
+        /// </summary>
+        /// <param name="petType">The pet type</param>
+        /// <returns><see cref="Task{PetsByPersonGenderCollection}"/></returns>
+        [HttpGet("petsbypersongender")]
         public async Task<PetsByPersonGenderCollection> GetPetsByPersonGender(PetType petType)
         {
             return await _petsManager.GetPetsByPersonGender(petType);
