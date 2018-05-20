7d7aaa247941586a42bd01911ce4db07dc78754a
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-20 11:39:58 +1000

    re-factored unit tests. handled injectable pets service and httpclient.

--- agl-web/src/app/components/agl-pets/agl-pets.component.spec.ts
+++ agl-web/src/app/components/agl-pets/agl-pets.component.spec.ts
@@ -1,19 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AglPetsComponent } from './agl-pets.component';
+import { PetsService } from '../../services/pets-service/pets-service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('AglPetsComponent', () => {
   let component: AglPetsComponent;
   let fixture: ComponentFixture<AglPetsComponent>;
+  let petsService: PetsService;
+  let http: HttpClient;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AglPetsComponent ]
+      declarations: [ AglPetsComponent ],
+      providers: [
+        PetsService,
+        HttpClient
+      ],
+      imports: [
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(() => {    
+    http = TestBed.get(HttpClient);
+    petsService = TestBed.get(PetsService) // Get the needed service    
     fixture = TestBed.createComponent(AglPetsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
